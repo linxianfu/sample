03b20407fb7e71d0bce35136354b7b352ec6feb1
linxianfu <linxianfu928@qq.com>
2017-08-17 07:32:29 +0000

    Finish static pages

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -6,21 +6,6 @@ use Illuminate\Http\Request;
 
 class StaticPagesController extends Controller
 {
-    // public function home()
-    // {
-    //     echo "主页";
-    // }
-    //
-    // public function help()
-    // {
-    //     echo "帮助页";
-    // }
-    //
-    // public function about()
-    // {
-    //     echo "关于页";
-    // }
-
     public function home()
     {
         return view('static_pages/home');
---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('title', 'Laravel入门教程')
+@section('content')
+    <h1>Laravel入门教程</h1>
+@stop
